1e054f3b6a7fbb050ebe692c7c85e8d18f0dc25c
Matt Marlow <thequantumblink@gmail.com>
2024-11-27 01:20:06 -0600

    Initial Commit

---
+++ CanvasApp/CanvasAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasAppApp.swift
+//  CanvasApp
+//
+//  Created by Matt Marlow on 11/27/24.
+//
+
+import SwiftUI
+
+@main
+struct CanvasAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
