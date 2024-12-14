fba61a91e0f64c3e30820ba144c01a9cf9419265
Matt <mm7f5@umsystem.edu>
2024-12-14 01:23:17 -0600

    implemented an enrollment client

--- CanvasApp/Clients/EnrollmentClient.swift
+++ CanvasApp/Clients/EnrollmentClient.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+struct EnrollmentClient {
+    func getCourseEnrollmentsForUser(from user: User) async throws -> [Enrollment] {
+        let userID = user.id!
+        
+        guard let url = URL(string: "\(baseURL)/users/\(String(userID))/enrollments?per_page=400") else {
+            throw NetworkError.badURL
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        var request = URLRequest(url: url)
+        request.addValue("Bearer " + APIToken, forHTTPHeaderField: "Authorization")
+        let (data, response) = try await URLSession.shared.data(for: request)
+        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == 200 else {
+            throw NetworkError.invalidResponse
+            }
+        do {
+                return try decoder.decode([Enrollment].self, from: data)
+            }
+        catch {
+                print("Decoding error: \(error)")
+                throw NetworkError.badDecode
+            }
+        
+        
+    }
+
+
+}
+
+
+//func getEnrollmentsForCourse(from course: Course) async throws -> [Enrollment] {
+//    return
+//}
+
