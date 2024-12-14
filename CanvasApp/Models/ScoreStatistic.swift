d2916e9ec40065669da475d8bb366302537fd197
Matt <mm7f5@umsystem.edu>
2024-12-14 01:21:35 -0600

    set scorestatistic to use floats instead

--- CanvasApp/Models/ScoreStatistic.swift
+++ CanvasApp/Models/ScoreStatistic.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+
+struct ScoreStatistic : Decodable {
+    var min: Float
+    var max: Float
+    var mean: Float
+    var upperQuartile: Float
+    var median: Float
+    var lowerQuartile: Float
+    
+    private enum CodingKeys: String, CodingKey {
+        case min
+        case max
+        case mean
+        case upperQuartile = "upper_q"
+        case median
+        case lowerQuartile = "lower_q"
+    }
+}
