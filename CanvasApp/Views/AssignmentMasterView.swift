501eee8b7341052763b41c1ac41df269f3f3f4c5
Matt <mm7f5@umsystem.edu>
2024-12-16 23:17:43 -0600

    added a full view for assignments

--- CanvasApp/Views/AssignmentMasterView.swift
+++ CanvasApp/Views/AssignmentMasterView.swift
@@ -13,6 +13,248 @@ struct ScoreDataPoint : Identifiable{
     let value: Float
     let type: String
 }
+@ViewBuilder
+func buildScoreStatistic(for assignment: Assignment, color: Color = .black) -> some View {
+        let scoreStatistic = assignment.scoreStatistic!
+        
+    let data: [ScoreDataPoint] = [
+            ScoreDataPoint(id: UUID(), name: "Max", value: scoreStatistic.max, type: "Computed"),
+            ScoreDataPoint(id: UUID(), name: "Min", value: scoreStatistic.min, type: "Computed"),
+            ScoreDataPoint(id: UUID(), name: "Mean", value: scoreStatistic.mean, type: "Computed"),
+            ScoreDataPoint(id: UUID(), name: "LowerQuart", value: scoreStatistic.lowerQuartile, type: "Quartile"),
+            ScoreDataPoint(id: UUID(), name: "UpperQuart", value: scoreStatistic.upperQuartile, type: "Quartile"),
+            ScoreDataPoint(id: UUID(), name: "Zero", value: 0, type: "Computed"),
+            
+            ScoreDataPoint(id: UUID(), name: "Score", value: Float((assignment.currentSubmission?.score)!), type: "Real")]
+            Chart(data) {
+               
+                if let maxValue = data.first(where: { $0.name == "Max" })?.value {
+                        RuleMark(
+                            xStart: .value("Min Value", 0),
+                            xEnd: .value("Max Value", maxValue),
+                            y: .value("Fixed", 0)
+                        )
+                        .foregroundStyle(Color.gray)
+                        .lineStyle(StrokeStyle(lineWidth: 2)) // Dashed line for emphasis
+                    }
+                if let minValue = data.first(where: { $0.name == "Min" })?.value,
+                       let maxValue = data.first(where: { $0.name == "Max" })?.value {
+                        RuleMark(
+                            xStart: .value("Min Value", minValue),
+                            xEnd: .value("Max Value", maxValue),
+                            y: .value("Fixed", 0)
+                        )
+                        .foregroundStyle(Color.black)
+                        .lineStyle(StrokeStyle(lineWidth: 2)) // Dashed line for emphasis
+                    }
+                if let minValue = data.first(where: { $0.name == "LowerQuart" })?.value,
+                       let maxValue = data.first(where: { $0.name == "UpperQuart" })?.value {
+                        RuleMark(
+                            xStart: .value("Min Value", minValue),
+                            xEnd: .value("Max Value", maxValue),
+                            y: .value("Fixed", 0)
+                        )
+                        .foregroundStyle(Color.blue)
+                        .lineStyle(StrokeStyle(lineWidth: 2)) // Dashed line for emphasis
+                    }
+                if ($0.type == "Computed") {
+                    PointMark(
+                        x: .value($0.name, $0.value)
+                    )
+                    .foregroundStyle(color)
+                    .symbol(.plus)
+                    .symbolSize(100)
+
+                }
+                else if ($0.type == "Quartile") {
+                    PointMark(
+                        x: .value($0.name, $0.value)
+                    )
+                    .foregroundStyle(.blue)
+                    .symbol(.diamond)
+                    .symbolSize(100)
+                }
+                else {
+                    PointMark(
+                        x: .value($0.name, $0.value)
+                    )
+                    .foregroundStyle(color)
+                    .symbolSize(150)
+                }
+                
+            }.frame(height: 40)
+            .chartYAxis(.hidden)
+            .chartXScale(domain: 0...scoreStatistic.max)
+            .chartXAxis {
+                AxisMarks(values: [scoreStatistic.min, scoreStatistic.mean, scoreStatistic.max]) { value in
+
+                    AxisTick()
+                        
+                }
+            }
+    HStack {
+        Text("Min: \(scoreStatistic.min.clean)")
+            .font(.caption)
+        Spacer()
+        Text("Mean: \(scoreStatistic.mean.clean)")
+            .font(.caption)
+
+        Spacer()
+        Text("Max: \(scoreStatistic.max.clean)")
+            .font(.caption)
+    }
+    HStack {
+        Text("Lower Quartile: \(scoreStatistic.lowerQuartile.clean)")
+            .font(.caption)
+        Spacer()
+        Text("Median: \(scoreStatistic.median.clean)")
+            .font(.caption)
+
+        Spacer()
+        Text("Upper Quartile: \(scoreStatistic.upperQuartile.clean)")
+            .font(.caption)
+    }
+
+    
+
+    
+}
+
+struct AssignmentPageView : View {
+    var courseWrapper: CourseWrapper
+    var assignment : Assignment
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    let color: Color
+    var loadGrades: Bool = false
+    
+    @Binding private var navigationPath: NavigationPath
+    
+    init(courseWrapper: CourseWrapper, assignment: Assignment, navigationPath: Binding<NavigationPath>) {
+        self.courseWrapper = courseWrapper
+        self.assignment = assignment
+        self.color = HexToColor(courseWrapper.course.color) ?? .black
+        self._navigationPath = navigationPath
+        loadGrades = assignment.currentSubmission?.grade != nil
+    }
+    @ViewBuilder
+    private func buildHeader() -> some View {
+        VStack(alignment: .leading) {
+            Text(assignment.title)
+                .font(.title)
+            HStack {
+                Text("\(assignment.pointsPossible?.clean ?? "0") pts")
+                    .font(.subheadline)
+                if loadGrades {
+                    Text("Graded")
+                        .font(.subheadline)
+                }
+            
+            }
+        }
+        .padding(.top)
+        .padding(.leading)
+    }
+    @ViewBuilder
+    private func buildAttemptView() -> some View {
+        VStack {
+            if assignment.currentSubmission != nil {
+                HStack {
+                    Text("Attempt: \(assignment.currentSubmission?.attempt ?? 0)")
+                        .font(.footnote)
+
+                    Spacer()
+                    Text(formattedDate(for: assignment.currentSubmission?.submittedAt ?? Date(), format: .longFormWithTime))
+                        .font(.footnote)
+
+                }
+            }
+            if (loadGrades) {
+                VStack {
+                    HStack {
+                        ZStack(alignment: .center) {
+                            Circle()
+                                .stroke(color, lineWidth: 5)
+                                .frame(width: 50, height: 50)
+                            Text(String(assignment.currentSubmission?.score?.clean ?? "0"))
+                            
+                        }
+                        Text("Out of \(assignment.pointsPossible?.clean ?? "0")")
+                        Spacer()
+                    }
+                    if (assignment.scoreStatistic != nil) {
+                        buildScoreStatistic(for: assignment, color: color)
+                    }
+                }.padding(.all)
+                    .border(.black, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                
+                    
+            }
+            
+        }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+    }
+    @ViewBuilder
+    private func buildDueAndSubType() -> some View {
+        VStack(alignment: .leading) {
+            Text("Due")
+                .font(.caption)
+            if (assignment.dueAt == nil) {
+                Text("No Due Date")
+            }
+            else {
+                Text("\(formattedDate(for: assignment.dueAt ?? Date(),format: .longFormWithTime))")
+                    .font(.caption)
+                    .fontWeight(.regular)
+                
+            }
+        }
+        .padding(.leading)
+    }
+    
+        
+    var body: some View {
+        VStack {
+            HStack {
+                buildHeader()
+                Spacer()
+            }
+            Divider()
+            buildAttemptView()
+                .padding(.top, 10)
+                .padding(.leading, 10)
+                .padding(.trailing, 10)
+            Divider()
+            HStack {
+                buildDueAndSubType()
+                    .padding(.top, 20)
+                Spacer()
+            }
+            
+            Divider()
+            preparePageDisplay(page: assignment)
+                .padding(.top)
+                .padding(.horizontal)
+            Spacer()
+            
+        }
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                GlobalTracking.BackButton(binding: presentationMode, navigationPath: $navigationPath)
+            }
+            ToolbarItem(placement: .principal) {
+                Text("Assignment Details")
+                    .foregroundStyle(.white)
+                    .font(.title)
+                    .fontWeight(.heavy)
+            }
+        }
+        .toolbarBackground(color, for: .navigationBar)
+        .toolbarBackground(.visible, for: .navigationBar)
+    }
+}
+
 
 
 struct AssignmentMasterView: View {
@@ -25,6 +267,9 @@ struct AssignmentMasterView: View {
     let color: Color
     
     @Binding private var navigationPath: NavigationPath
+    
+    @State private var selectedAssignment: Assignment
+    @State private var loadAssignmentPage: Bool = false
     
     var assignmentDates: [Assignment : String] = [ : ]
     
@@ -39,117 +284,12 @@ struct AssignmentMasterView: View {
             let formattedDate = formattedDate(for: assignment.dueAt ?? Date(), format: .longFormWithTime)
             assignmentDates.updateValue(formattedDate, forKey: assignment)
         }
-       
+        selectedAssignment = courseWrapper.course.assignments[0]
         
     
         
     }
-    @ViewBuilder
-    private func buildScoreStatistic(for assignment: Assignment) -> some View {
-            let scoreStatistic = assignment.scoreStatistic!
-            
-        let data: [ScoreDataPoint] = [
-                ScoreDataPoint(id: UUID(), name: "Max", value: scoreStatistic.max, type: "Computed"),
-                ScoreDataPoint(id: UUID(), name: "Min", value: scoreStatistic.min, type: "Computed"),
-                ScoreDataPoint(id: UUID(), name: "Mean", value: scoreStatistic.mean, type: "Computed"),
-                ScoreDataPoint(id: UUID(), name: "LowerQuart", value: scoreStatistic.lowerQuartile, type: "Quartile"),
-                ScoreDataPoint(id: UUID(), name: "UpperQuart", value: scoreStatistic.upperQuartile, type: "Quartile"),
-                ScoreDataPoint(id: UUID(), name: "Zero", value: 0, type: "Computed"),
-                
-                ScoreDataPoint(id: UUID(), name: "Score", value: Float((assignment.currentSubmission?.score)!), type: "Real")]
-                Chart(data) {
-                   
-                    if let maxValue = data.first(where: { $0.name == "Max" })?.value {
-                            RuleMark(
-                                xStart: .value("Min Value", 0),
-                                xEnd: .value("Max Value", maxValue),
-                                y: .value("Fixed", 0)
-                            )
-                            .foregroundStyle(Color.gray)
-                            .lineStyle(StrokeStyle(lineWidth: 2)) // Dashed line for emphasis
-                        }
-                    if let minValue = data.first(where: { $0.name == "Min" })?.value,
-                           let maxValue = data.first(where: { $0.name == "Max" })?.value {
-                            RuleMark(
-                                xStart: .value("Min Value", minValue),
-                                xEnd: .value("Max Value", maxValue),
-                                y: .value("Fixed", 0)
-                            )
-                            .foregroundStyle(Color.black)
-                            .lineStyle(StrokeStyle(lineWidth: 2)) // Dashed line for emphasis
-                        }
-                    if let minValue = data.first(where: { $0.name == "LowerQuart" })?.value,
-                           let maxValue = data.first(where: { $0.name == "UpperQuart" })?.value {
-                            RuleMark(
-                                xStart: .value("Min Value", minValue),
-                                xEnd: .value("Max Value", maxValue),
-                                y: .value("Fixed", 0)
-                            )
-                            .foregroundStyle(Color.blue)
-                            .lineStyle(StrokeStyle(lineWidth: 2)) // Dashed line for emphasis
-                        }
-                    if ($0.type == "Computed") {
-                        PointMark(
-                            x: .value($0.name, $0.value)
-                        )
-                        .foregroundStyle(color)
-                        .symbol(.plus)
-                        .symbolSize(100)
-
-                    }
-                    else if ($0.type == "Quartile") {
-                        PointMark(
-                            x: .value($0.name, $0.value)
-                        )
-                        .foregroundStyle(.blue)
-                        .symbol(.diamond)
-                        .symbolSize(100)
-                    }
-                    else {
-                        PointMark(
-                            x: .value($0.name, $0.value)
-                        )
-                        .foregroundStyle(color)
-                        .symbolSize(150)
-                    }
-                    
-                }.frame(height: 40)
-                .chartYAxis(.hidden)
-                .chartXScale(domain: 0...scoreStatistic.max)
-                .chartXAxis {
-                    AxisMarks(values: [scoreStatistic.min, scoreStatistic.mean, scoreStatistic.max]) { value in
-
-                        AxisTick()
-                            
-                    }
-                }
-        HStack {
-            Text("Min: \(scoreStatistic.min, specifier: "%.2f")")
-                .font(.caption)
-            Spacer()
-            Text("Mean: \(scoreStatistic.mean, specifier: "%.2f")")
-                .font(.caption)
-
-            Spacer()
-            Text("Max: \(scoreStatistic.max, specifier: "%.2f")")
-                .font(.caption)
-        }
-        HStack {
-            Text("Lower Quartile: \(scoreStatistic.lowerQuartile, specifier: "%.2f")")
-                .font(.caption)
-            Spacer()
-            Text("Median: \(scoreStatistic.median, specifier: "%.2f")")
-                .font(.caption)
-
-            Spacer()
-            Text("Upper Quartile: \(scoreStatistic.upperQuartile, specifier: "%.2f")")
-                .font(.caption)
-        }
-
-        
-
-        
-    }
+    
     @ViewBuilder
     private func buildAssignmentGlanceView(for assignment: Assignment) -> some View {
 //        GeometryReader { geometry in
@@ -161,7 +301,7 @@ struct AssignmentMasterView: View {
                         Circle()
                             .stroke(color, lineWidth: 5)
                             .frame(width: 50, height: 50)
-                        Text(String(assignment.pointsPossible ?? 0))
+                        Text(String(assignment.pointsPossible?.clean ?? "0"))
                     }
                     Text("Points Possible")
                     Spacer()
@@ -176,18 +316,18 @@ struct AssignmentMasterView: View {
                         Circle()
                             .stroke(color, lineWidth: 5)
                             .frame(width: 50, height: 50)
-                        Text(String(assignment.currentSubmission?.score ?? 0))
+                        Text(String(assignment.currentSubmission?.score?.clean ?? "0"))
                     }
                     .contextMenu(ContextMenu(menuItems: {
                         /*@START_MENU_TOKEN@*/Text("Menu Item 1")/*@END_MENU_TOKEN@*/
                         
                     }))
-                    Text("Out of \(assignment.pointsPossible!, specifier: "%.1f")")
+                    Text("Out of \(assignment.pointsPossible?.clean ?? "0")")
                     Spacer()
                 }
                 
                 if assignment.scoreStatistic != nil {
-                    buildScoreStatistic(for: assignment)
+                    buildScoreStatistic(for: assignment, color: color)
                     
 
                 }
@@ -247,7 +387,7 @@ struct AssignmentMasterView: View {
                                                 .resizable()
                                                 .frame(width: 20, height: 15)                                        
                                                 .foregroundStyle(.green)
-                                        }.padding(.top, 4)
+                                        }.padding(.top, 8)
                                             .padding(.trailing, 10)
                                        
 
@@ -268,7 +408,10 @@ struct AssignmentMasterView: View {
                     }
                     
                 }
-                )
+                ).simultaneousGesture(LongPressGesture().onEnded {_ in
+                    selectedAssignment = assignment
+                    loadAssignmentPage = true
+                })
                 .tint(HexToColor(courseWrapper.course.color))
             }
     }
@@ -285,7 +428,7 @@ struct AssignmentMasterView: View {
                         .foregroundStyle(.white)
                    
                 }
-                Text(String(courseWrapper.course.enrollment?.grade?.currentScore ?? 0.00) + "%")
+                Text(String(courseWrapper.course.enrollment?.grade?.currentScore?.clean ?? "0") + "%")
                     .font(.title2)
                     .foregroundStyle(.white)
             }
@@ -344,7 +487,9 @@ struct AssignmentMasterView: View {
             VStack {
                 buildGradeHeader()
                 buildAssignmentList()
-            }
+            }.navigationDestination(isPresented: $loadAssignmentPage, destination: {
+                AssignmentPageView(courseWrapper: courseWrapper, assignment: selectedAssignment, navigationPath: $navigationPath)
+            })
         .navigationBarBackButtonHidden(true)
         .toolbar {
             ToolbarItem(placement: .topBarLeading) {
