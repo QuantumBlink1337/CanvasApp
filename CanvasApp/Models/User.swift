73a0fbe35256223c7ce3eb1b211c50acd1cf8195
Matt <mm7f5@umsystem.edu>
2024-12-21 20:37:04 -0600

    added encoding and decoding for user

--- CanvasApp/Models/User.swift
+++ CanvasApp/Models/User.swift
@@ -31,7 +31,34 @@ struct User: Codable, Identifiable {
         case pronouns
         case avatarURL = "avatar_url"
         case name = "name"
+        case enrollments
     }
+    
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decodeIfPresent(Int.self, forKey: .id)
+        self.firstName = try container.decodeIfPresent(String.self, forKey: .firstName)
+        self.lastName = try container.decodeIfPresent(String.self, forKey: .lastName)
+        self.displayName = try container.decodeIfPresent(String.self, forKey: .displayName)
+        self.pronouns = try container.decodeIfPresent(String.self, forKey: .pronouns)
+        self.avatarURL = try container.decodeIfPresent(String.self, forKey: .avatarURL)
+        self.name = try container.decodeIfPresent(String.self, forKey: .name)
+        self.enrollments = try container.decodeIfPresent([Enrollment].self, forKey: .enrollments) ?? []
+    }
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encodeIfPresent(self.id, forKey: .id)
+        try container.encodeIfPresent(self.firstName, forKey: .firstName)
+        try container.encodeIfPresent(self.lastName, forKey: .lastName)
+        try container.encodeIfPresent(self.displayName, forKey: .displayName)
+        try container.encodeIfPresent(self.pronouns, forKey: .pronouns)
+        try container.encodeIfPresent(self.avatarURL    , forKey: .avatarURL)
+        try container.encodeIfPresent(self.name, forKey: .name)
+        try container.encode(self.enrollments, forKey: .enrollments)
+    }
+    
+    
 }
 struct UserColorCodes: Codable {
     var customColors: [String: String]
